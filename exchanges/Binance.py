157cc739ab2fa2081f9283978f0beecd49a3f08a
Luke Jones <underdweller@gmail.com>
2018-08-23 14:28:41 -0700

    Code cleanup and responsibility separation

--- exchanges/Binance.py
+++ exchanges/Binance.py
@@ -1,7 +1,14 @@
-from .BaseExchange import BaseExchange
+from .BaseExchange import *
+
+
+# set default options for binance
+ccxt.binance.options['newOrderRespType'] = 'FULL'
+ccxt.binance.options['defaultTimeInForce'] = 'IOC'
+ccxt.binance.options['parseOrderToPrecision'] = True
+ccxt.binance.options['recvWindow'] = 10000
 
 
 class BinanceExchange(BaseExchange):
 
     def __init__(self):
-        super(self)  # Whatever this is
+        super().__init__(exchange_id=None, access_keys=typing.Dict[str, str])

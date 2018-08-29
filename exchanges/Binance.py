8f0e834c014703095641cb9271d97ebf9417c644
K-Kit <kylekitlinski@gmail.com>
2018-08-29 00:30:37 -0400

    some comments - will improve tomorrow

--- exchanges/Binance.py
+++ exchanges/Binance.py
@@ -17,27 +17,28 @@ class BinanceExchange(BaseExchange):
 
     def __init__(self, exchange_id, access_keys):
         super().__init__(exchange_id=exchange_id, access_keys=access_keys)
-        self.res = []
-        self.rest = []
-        self.resc = []
-        self.last_update_time = None
 
     def init_client_connection(self):
         super().init_client_connection()
 
         # =========================================
         # set default options for binance
+
+        # FULL order response to include trade list
         self.client.options['newOrderRespType'] = 'FULL'
+        # default order time IMMEDIATE OR CANCEL
         self.client.options['defaultTimeInForce'] = 'IOC'
+        # so we dont have to mess with foat/str precision per pair
         self.client.options['parseOrderToPrecision'] = True
         self.client.options['recvWindow'] = 10000
 
     def init_socket_manager(self, public, secret):
         self.socket_manager = BinanceSocketManager(Client(public, secret))
-        # self.socket_manager.start()
+        self.socket_manager.start()
 
     def process_multiplex_socket(self, msg):
-
+        # this callback recieves all socket messages and dispatches to the appropriate handler function
+        # todo add error handling, restart, check for last_update time lag on each socket
         if 'e' in msg and msg['e'] == 'error':
             print('implement socket error handleing', msg)
             return
@@ -45,15 +46,12 @@ class BinanceExchange(BaseExchange):
         if 'kline' in msg['stream']:
             self.handle_candle_socket(msg['data'], self.parse_stream_name(msg['stream']), self.parse_candle_period(msg['stream']))
             self.last_candle_update_time = time.time()
-            self.resc.append(msg['data'])
 
         elif 'depth' in msg['stream']:
-            self.res.append(msg['data'])
             self.last_depth_update_time = time.time()
             self.handle_depth_socket(msg['data'], self.parse_stream_name(msg['stream']))
 
         elif 'ticker' in msg['stream']:
-            self.rest.append(msg['data'])
             self.last_ticker_update_time = time.time()
             self.handle_ticker_socket(msg['data'], self.parse_stream_name(msg['stream']))
 
@@ -64,6 +62,8 @@ class BinanceExchange(BaseExchange):
 
 
     def handle_candle_socket(self, msg, symbol, candle_period):
+
+        # update candlestick data for appropriate candle_period
         if 'e' in msg and msg['e'] == 'error':
             print('implement socket error handleing', msg)
             return
@@ -72,6 +72,7 @@ class BinanceExchange(BaseExchange):
 
 
     def handle_ticker_socket(self, msg, symbol):
+        # renamed what used to be pair.price to pair['close'] to follow CCXT conventions
         if 'e' in msg and msg['e'] == 'error':
             print('implement socket error handleing', msg)
             return
@@ -81,18 +82,38 @@ class BinanceExchange(BaseExchange):
             self.pairs[symbol]['percentage'] = msg['P']
 
     def handle_depth_socket(self, msg, symbol):
+        # update bids/asks: TODO needs to be parsed to floats instead of strings
         if 'e' in msg and msg['e'] == 'error':
             print('implement socket error handleing', msg)
             return
         if symbol in self.pairs: self.pairs[symbol]['orderbook'] = msg
 
 
-    def save(self):
-        with open('../pairs_data.json', 'w') as f:
-            json.dump(self.pairs, f)
+    def start(self):
+        self.init_client_connection()
+        self.init_socket_manager(keys.public, keys.secret)
+        self.pairs = self.get_pairs('USDT')
+        # timeframes hardcoded for now will be changed once we have a config
+        timeframes = ['5m', '15m']
+        asyncio.get_event_loop().run_until_complete(
+            self.load_all_candle_histories(timeframes=timeframes, num_candles=200))
+        time.sleep(1)
+        # generate list of stream names to start in multiplex socket
+        candle_sockets, depth_sockets, ticker_sockets = gen_socket_list(self.pairs, timeframes)
+
+        # store connection keys self.candle_sock
+        self.candle_socket = self.socket_manager.start_multiplex_socket(candle_sockets, self.process_multiplex_socket)
+        time.sleep(1)
+
+        self.depth_socket = self.socket_manager.start_multiplex_socket(depth_sockets, self.process_multiplex_socket)
+
+        time.sleep(1)
+        self.ticker_socket = self.socket_manager.start_multiplex_socket(ticker_sockets, self.process_multiplex_socket)
 
     @staticmethod
     def parse_stream_name(stream_name):
+        # split the stream name to get and format symbol for dict access
+        # need to find better way to fix removed / changed names for main net swaps
         stream = stream_name.split('@')[0]
         market = stream[-3:] if stream[-1] != 't' else stream[-4:]
         symbol = stream[:-len(market)]
@@ -104,6 +125,7 @@ class BinanceExchange(BaseExchange):
 
     @staticmethod
     def parse_candle_period(stream_name):
+        # get candle period from stream name to associate candle period
         return stream_name.split('_')[1]
 
 
@@ -115,32 +137,4 @@ class keys:
 
 if __name__ == '__main__':
     ex = BinanceExchange('binance', {'public': keys.public, 'secret': keys.secret})
-    ex.init_client_connection()
-    ex.init_socket_manager(keys.public, keys.secret)
-    ex.pairs = ex.get_pairs('USDT')
-    print(ex.pairs.keys())
-    print(len(ex.pairs))
-    timeframes = ['5m', '15m']
-    asyncio.get_event_loop().run_until_complete(ex.load_all_candle_histories(timeframes=timeframes, num_candles=200))
-    time.sleep(1)
-    candle_sockets, depth_sockets, ticker_sockets = gen_socket_list(ex.pairs, timeframes)
-
-    ex.socket_manager.start()
-
-    ex.socket_manager.start_multiplex_socket(candle_sockets, ex.process_multiplex_socket)
-    time.sleep(1)
-
-    ex.socket_manager.start_multiplex_socket(depth_sockets, ex.process_multiplex_socket)
-
-    time.sleep(1)
-    ex.socket_manager.start_multiplex_socket(ticker_sockets, ex.process_multiplex_socket)
-    # from timeit import default_timer as timer
-    #
-    # start = timer()
-    # # ...
-    #
-    # asyncio.get_event_loop().run_until_complete(ex.load_all_candle_histories(timeframes= ['5m', '15m'], num_candles=100))
-    # end = timer()
-    #
-    # print(len(ex.pairs))
-    # print(end - start)
+    ex.start()

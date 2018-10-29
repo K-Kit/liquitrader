ae57591a74583072c726374f0ec86a9af6812f90
Luke Jones <underdweller@gmail.com>
2018-10-29 11:54:03 -0700

    Performance improvements, code cleanup, renaming

--- webserver/webserver.py
+++ webserver/webserver.py
@@ -5,7 +5,7 @@ from flask import jsonify
 
 app = Flask(__name__)
 
-BP_ENGINE = None
+LT_TRADER = None
 
 @app.route("/")
 def gethello():
@@ -14,14 +14,14 @@ def gethello():
 
 @app.route("/holding")
 def get_holding():
-    df = BP_ENGINE.pairs_to_df(friendly=True)
+    df = LT_TRADER.pairs_to_df(friendly=True)
     df[df['Amount'] > 0].to_json(orient='records', path_or_buf='holding')
     return jsonify(df[df['Amount'] > 0].to_json(orient='records'))
 
 
 @app.route("/market")
 def get_market():
-    df = BP_ENGINE.pairs_to_df(friendly=True)
+    df = LT_TRADER.pairs_to_df(friendly=True)
     df[FRIENDLY_MARKET_COLUMNS].to_json(orient='records', path_or_buf='market')
 
     return jsonify(df[FRIENDLY_MARKET_COLUMNS].to_json(orient='records'))
@@ -29,7 +29,7 @@ def get_market():
 
 @app.route("/buy_log")
 def get_buy_log_frame():
-    df = pd.DataFrame(BP_ENGINE.trade_history)
+    df = pd.DataFrame(LT_TRADER.trade_history)
     df['gain'] = (df.price - df.bought_price) / df.bought_price * 100
     df['net_gain'] = (df.price - df.bought_price) * df.filled
     cols = ['datetime', 'symbol', 'bought_price', 'price', 'amount', 'side', 'status', 'remaining', 'filled', 'gain']
@@ -40,7 +40,7 @@ def get_buy_log_frame():
 
 @app.route("/sell_log")
 def get_sell_log_frame():
-    df = pd.DataFrame(BP_ENGINE.trade_history)
+    df = pd.DataFrame(LT_TRADER.trade_history)
     df['gain'] = (df.price - df.bought_price) / df.bought_price * 100
     cols = ['datetime', 'symbol', 'bought_price', 'price', 'amount', 'side', 'status', 'remaining', 'filled', 'gain']
     df[df.side == 'sell'][cols].to_json(orient='records', path_or_buf='sell_log')
@@ -50,15 +50,15 @@ def get_sell_log_frame():
 @app.route("/dashboard_data")
 def get_dashboard_data():
     data = {
-        "quote_balance": BP_ENGINE.exchange.balance,
-        "total_pending_value": BP_ENGINE.get_pending_value(),
-        "total_current_value": BP_ENGINE.get_tcv(),
-        "total_profit": BP_ENGINE.get_total_profit(),
-        "total_profit_percent": BP_ENGINE.get_total_profit() / BP_ENGINE.exchange.balance * 100,
-        "daily_profit_data": BP_ENGINE.get_daily_profit_data().to_json(orient='records'),
-        "holding_chart_data": BP_ENGINE.pairs_to_df()['total_cost'].dropna().to_json(orient='records'),
-        "cum_profit": BP_ENGINE.get_cumulative_profit().to_json(orient='records'),
-        "pair_profit_data": BP_ENGINE.get_pair_profit_data().to_json(orient='records')
+        "quote_balance": LT_TRADER.exchange.balance,
+        "total_pending_value": LT_TRADER.get_pending_value(),
+        "total_current_value": LT_TRADER.get_tcv(),
+        "total_profit": LT_TRADER.get_total_profit(),
+        "total_profit_percent": LT_TRADER.get_total_profit() / LT_TRADER.exchange.balance * 100,
+        "daily_profit_data": LT_TRADER.get_daily_profit_data().to_json(orient='records'),
+        "holding_chart_data": LT_TRADER.pairs_to_df()['total_cost'].dropna().to_json(orient='records'),
+        "cum_profit": LT_TRADER.get_cumulative_profit().to_json(orient='records'),
+        "pair_profit_data": LT_TRADER.get_pair_profit_data().to_json(orient='records')
     }
 
     return data

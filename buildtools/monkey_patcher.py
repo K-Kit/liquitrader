45c47aea884b033c2e80f883e29aade0d30cb625
Luke Jones <underdweller@gmail.com>
2018-11-09 13:37:52 -0800

    Separate build process into functions

--- buildtools/monkey_patcher.py
+++ buildtools/monkey_patcher.py
@@ -1,31 +1,60 @@
-import os
 import sys
 from distutils.sysconfig import get_python_lib
 import site
 
-def patch_strptime_patcher():
-    if sys.platform == 'win32':
-        found = False
+import tempfile
+import zipfile
+import shutil
+import os
 
-        for lib_path in (site.getusersitepackages(), get_python_lib(True)):
-            strptime_monkeypatch_path = os.path.join(lib_path, 'dateparser', 'utils', 'strptime.py')
 
-            if os.path.exists(strptime_monkeypatch_path):
-                found = True
-                break
+def remove_from_zip(zipfname, *filenames):
+    tempdir = tempfile.mkdtemp()
 
-        if not found:
-            print('ERROR: Could not find dateparser/utils/strptime.py in site packages')
-            sys.exit(1)
+    try:
+        tempname = os.path.join(tempdir, 'new.zip')
 
-    else:
-        strptime_monkeypatch_path = '/usr/local/lib/python3.6/dist-packages'
-        strptime_monkeypatch_path = os.path.join(strptime_monkeypatch_path, 'dateparser', 'utils', 'strptime.py')
+        with zipfile.ZipFile(zipfname, 'r') as zipread:
+            with zipfile.ZipFile(tempname, 'w') as zipwrite:
+                for item in zipread.infolist():
+                    if item.filename not in filenames:
+                        data = zipread.read(item.filename)
+                        zipwrite.writestr(item, data)
 
+        shutil.move(tempname, zipfname)
+
+    finally:
+        shutil.rmtree(tempdir)
+
+
+def strptime_patcher():
+    #if sys.platform == 'win32':
+    found = False
+
+    for lib_path in (site.getusersitepackages(), get_python_lib(True)):
+        strptime_monkeypatch_path = os.path.join(lib_path, 'dateparser', 'utils', 'strptime.py')
+
+        if os.path.exists(strptime_monkeypatch_path):
+            found = True
+            break
+
+    if not found:
+        print('ERROR: Could not find dateparser/utils/strptime.py in site packages')
+        sys.exit(1)
+
+    # else:
+    #     paths = [
+    #         '/usr/local/lib/python3.6/dist-packages/dateparser/utils/strptime.py',
+    #
+    #     strptime_monkeypatch_path =
+    #
+    #     if not os.path.isfile(strptime_monkeypatch_path):
+    #         strptime_monkeypatch_path = '/usr/local/lib/python3.6/site-packages'
+    #         strptime_monkeypatch_path = os.path.join(strptime_monkeypatch_path, 'dateparser', 'utils', 'strptime.py')
 
     with open(strptime_monkeypatch_path, 'r') as f:
         data = f.read()
-        
+
     # Patch already done
     if 'from zipimport' in data:
         return
@@ -63,9 +92,9 @@ except ZipImportError:
         f.write(data)
 
 
-def do_patches():
-    patch_strptime_patcher()
+def do_prebuild_patches():
+    strptime_patcher()
 
 
-if __name__ == '__main__':
-    do_patches()
+def do_postbuild_patches():
+    pass

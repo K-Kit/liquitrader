4561f0b212745a7c61563e5a58b8220fafeb0732
K-Kit <kylekitlinski@gmail.com>
2018-12-11 02:27:01 -0500

    fix gain calc

--- liquitrader.py
+++ liquitrader.py
@@ -549,7 +549,7 @@ class LiquiTrader:
         #     print(f'error parsing timezone in pairs to df {ex}')
         if 'total_cost' in df and 'close' in df:
             df['current_value'] = df.close * df.total * (1 - (fee / 100))
-            df['gain'] = (df.close - df.avg_price) / df.avg_price * 100 - fee
+            df['gain'] = (df.bid - df.avg_price) / df.avg_price * 100 - fee
 
         if friendly:
             try:

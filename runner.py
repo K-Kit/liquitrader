893a48ac0869634cdd813f76e58a1e06e620b31d
Luke Jones <underdweller@gmail.com>
2019-01-29 12:34:37 -0800

    Remove system-specific path parts from tracebacks, add critical fault crash logging

--- runner.py
+++ runner.py
@@ -1,3 +1,12 @@
+import sys
+
+if hasattr(sys, 'frozen'):
+    import utils.runtime_handler
+
+    utils.runtime_handler.enable_traceback_hook()  # Enable custom traceback handling (to strip build path info)
+    utils.runtime_handler.enable_faulthandler()  # Handles crashes that otherwise would be lost
+
+
 if __name__ == '__main__':
     import liquitrader
     liquitrader.main()

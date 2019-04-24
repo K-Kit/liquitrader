8cea6a72ab6d05091ca37fa2d8f26fb42eaebbbc
K-Kit <kylekitlinski@gmail.com>
2019-04-24 15:30:53 -0400

    Remove verifiers

--- dev_keys_binance.py
+++
@@ -1,6 +0,0 @@
-class keys:
-    """
-    The keys class
-    """
-    public = 'tNyFVXIKHOq2DctsqNlSEigFKRGBcPvQX4KIOUlLnPtwpHhPMYLCSIiZKRkVUOjj'
-    secret = 'E24LEdL2K4fdkA3QsJta1CerCkn5RTUotRqlJnsGMsCCwfqFBMJOGpeG7u3fFeIV'
